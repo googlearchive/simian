6eb03a8e9eee2f0c438ba6f7bfb76073a9bb7e3c
jrand@google.com <jrand@google.com@368532df-a33e-f994-cbfd-937b2e4ba68d>
2012-03-02 22:25:19 +0000

    1.5.3
    git-svn-id: https://simian.googlecode.com/svn/trunk@34 368532df-a33e-f994-cbfd-937b2e4ba68d

--- setup.py
+++ setup.py
@@ -1,19 +1,6 @@
 #!/usr/bin/env python
-# 
+#
 # Copyright 2011 Google Inc. All Rights Reserved.
-# 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# 
-#     http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS-IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# #
 
 try:
   from setuptools import setup, find_packages
@@ -26,12 +13,12 @@ except ImportError:
 REQUIRE = [
     'setuptools>=0.6c9',     # fix bugs with old version on Leopard
     'google_apputils>=0.2',
-    'python-dateutil>=1.4',  # because of google_apputils
+    'python-dateutil>=1.4,<2',  # because of google_apputils
     'pyasn1',
-    'tlslite',
     'M2Crypto',
     'pyyaml',
     'icalendar',
+    'tlslite==0.3.8',
 ]
 
 SIMIAN_STUBS = [
@@ -42,7 +29,7 @@ SIMIAN_ENTRY_POINTS = ['%s = simian.stubs:%s' % s for s in SIMIAN_STUBS]
 
 setup(
   name = 'simian',
-  version = '1.5.2',
+  version = '1.5.3',
   url = 'http://code.google.com/p/simian',
   license = 'Apache 2.0',
   description = 'An App Engine-based client & server component for Munki',
--- src/simian/util/link_module.sh
+++ src/simian/util/link_module.sh
@@ -15,16 +15,31 @@ except ImportError:
 EOF
 }
 
-
-function link_module() {
-  local module="$1"
-  local path=`find_module $module`
-  if [ ! -z "$path" ]; then
-    rm -f "$GAE_BUNDLE/$module"
-    ln -s "$path" "$GAE_BUNDLE/$module"
-  else
-    echo ERROR: path not found for $module. symlink creation failure.
+function find_egg() {
+  egg=$(find ${module}-*.egg -type f 2>/dev/null)
+  if [[ ! -z "${egg}" ]]; then
+    echo "${egg}"
   fi
 }
 
-link_module $1
+function link_module() {
+  local module="$1"
+  
+  local egg=$(find_egg ${module})
+  if [[ ! -z "${egg}" ]]; then
+    unzip -o "${egg}" -d "${GAE_BUNDLE}" > /dev/null
+    rm -rf "${GAE_BUNDLE}/EGG-INFO"
+    return
+  fi
+  
+  local path=$(find_module ${module})
+  if [[ ! -z "${path}" ]]; then
+    rm -f "${GAE_BUNDLE}/${module}"
+    ln -s "${path}" "${GAE_BUNDLE}/${module}"
+    return
+  fi
+
+  echo "ERROR: path not found for ${module}. symlink creation failure."
+}
+
+link_module "$1"
