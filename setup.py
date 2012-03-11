ae54a1e39331d62f04bfc9afe0ac30d99ba27b5e
ogle@google.com <ogle@google.com@368532df-a33e-f994-cbfd-937b2e4ba68d>
2012-03-11 12:57:56 +0000

    Commit postflight and setup.py for Simian 2.0 which somehow were omitted from the initial checkin...
    git-svn-id: https://simian.googlecode.com/svn/trunk@69 368532df-a33e-f994-cbfd-937b2e4ba68d

--- setup.py
+++ setup.py
@@ -1,6 +1,33 @@
 #!/usr/bin/env python
-#
+# 
 # Copyright 2011 Google Inc. All Rights Reserved.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# # Do NOT change the above sha-bang line unless if you know what you are doing.
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# #
 
 try:
   from setuptools import setup, find_packages
@@ -14,11 +41,13 @@ REQUIRE = [
     'setuptools>=0.6c9',     # fix bugs with old version on Leopard
     'google_apputils>=0.2',
     'python-dateutil>=1.4,<2',  # because of google_apputils
-    'pyasn1',
-    'M2Crypto',
-    'pyyaml',
-    'icalendar',
+    'pyasn1==0.1.2',
     'tlslite==0.3.8',
+    'M2Crypto==0.21.1',
+    'pyyaml==3.05',
+    'unittest2==0.5.1',
+    'icalendar==1.2',
+    'WebOb==1.1.1',
 ]
 
 SIMIAN_STUBS = [
@@ -29,7 +58,7 @@ SIMIAN_ENTRY_POINTS = ['%s = simian.stubs:%s' % s for s in SIMIAN_STUBS]
 
 setup(
   name = 'simian',
-  version = '1.5.3',
+  version = '2.0',
   url = 'http://code.google.com/p/simian',
   license = 'Apache 2.0',
   description = 'An App Engine-based client & server component for Munki',
