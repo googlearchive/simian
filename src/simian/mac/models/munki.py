881b1a75f4af269b75e0e19e28d0baf6959f77f2
ogle@google.com <ogle@google.com@368532df-a33e-f994-cbfd-937b2e4ba68d>
2012-03-30 15:46:58 +0000

    Recommit missing description fix.
    git-svn-id: https://simian.googlecode.com/svn/trunk@174 368532df-a33e-f994-cbfd-937b2e4ba68d

--- src/simian/mac/models/munki.py
+++ src/simian/mac/models/munki.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
-# 
+#
 # Copyright 2012 Google Inc. All Rights Reserved.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS-IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -266,7 +266,7 @@ class PackageInfo(BaseMunkiModel):
       self.plist['description'] = desc
     else:
       # Otherwise append the old avg duration text to the new description.
-      match = self.AVG_DURATION_REGEX.search(self.plist['description'])
+      match = self.AVG_DURATION_REGEX.search(self.plist.get('description', ''))
       if match:
         self.plist['description'] = '%s\n\n%s' % (desc, match.group(0))
       else:
