1c8d1a41b98be82346e4eb88dd547f8f5e4e53a3
ogle@google.com <ogle@google.com@368532df-a33e-f994-cbfd-937b2e4ba68d>
2013-04-02 22:48:21 +0000

    More permanent fix for ""You must either define the environment variable DJANGO_SETTINGS_MODULE or call settings.configure() ..."
    git-svn-id: https://simian.googlecode.com/svn/trunk@216 368532df-a33e-f994-cbfd-937b2e4ba68d

--- src/tests/simian/mac/cron/reports_cache_test.py
+++ src/tests/simian/mac/cron/reports_cache_test.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
-# 
+#
 # Copyright 2010 Google Inc. All Rights Reserved.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS-IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,6 +24,8 @@ import logging
 logging.basicConfig(filename='/dev/null')
 import random
 
+from django.conf import settings
+settings.configure()
 from google.apputils import app
 from google.apputils import basetest
 import mox
