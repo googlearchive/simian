fd7a49033799a49e9362a402dfbb66076d1f4407
ogle@google.com <ogle@google.com@368532df-a33e-f994-cbfd-937b2e4ba68d>
2013-12-06 22:58:39 +0000

    small test fix after latest update.
    git-svn-id: https://simian.googlecode.com/svn/trunk@245 368532df-a33e-f994-cbfd-937b2e4ba68d

--- src/tests/simian/mac/common/auth_test.py
+++ src/tests/simian/mac/common/auth_test.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
-# 
+#
 # Copyright 2010 Google Inc. All Rights Reserved.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS-IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -62,6 +62,7 @@ class AuthModuleTest(mox.MoxTestBase):
     self.mox.VerifyAll()
 
   def testDoUserAuthWithIsAdminTrueSuccess(self):
+    self.stubs.Set(auth.settings, 'ALLOW_ALL_DOMAIN_USERS_READ_ACCESS', False)
     self.stubs.Set(auth, 'users', self.mox.CreateMock(auth.users))
     self.mox.StubOutWithMock(auth, 'IsAdminUser')
     mock_user = self.mox.CreateMockAnything()
